adb9981bb215adb546bf23b4ce965a327fb90f3d
Joven <jaybenrosales12@gmail.com>
2026-03-14 22:56:42 +0800

    built the Dashboard, and additional backend route

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/inventory')
+def inventory():
+    return render_template('index.html')
+
+@app.route('/audit')
+def audit():
+    return render_template('index.html')
+
+@app.route('/history')
+def history():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
