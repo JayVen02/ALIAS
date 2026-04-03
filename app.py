e4fd60c3803b954aa37f25432e113f639e1a2caf
jinnSIBS <20210021519@my.xu.edu.ph>
2026-04-03 18:33:06 +0800

    app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html') #change the file to its destination file
 
 @app.route('/inventory')
 def inventory():
-    return render_template('index.html')
+    return render_template('index.html') #change the file to its destination file
 
 @app.route('/audit')
 def audit():
-    return render_template('index.html')
+    return render_template('index.html') #change the file to its destination file
 
 @app.route('/history')
-def history():
+def history():  
     return render_template('history.html')
 
 if __name__ == '__main__':
